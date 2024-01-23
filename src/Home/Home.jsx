60790a30ecf8b1a825295b9ffaea775c9f230338
imrulkayes75 <ikbubt@gmail.com>
2024-01-24 00:32:41 +0600

    added header image

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -6,16 +6,19 @@ import RightSideNav from '../Shared/RigntSideNav/RightSideNav';
 
 const Home = () => {
     return (
-        <div>
+        <div className='text-center'>
             <Header></Header>
             <Navbar></Navbar>
-            <h1 className='text-3xl font-popience'>This is home </h1>
-            <div>
-                <div><LeftSideNav></LeftSideNav></div>
-                <div>
+            <div className='grid grid-cols-1 md:grid-cols-4 gap-6'>
+                <div className='border'>
+                    <LeftSideNav></LeftSideNav>
+                </div>
+                <div className='grid col-span-2 border'>
                     <h2>News comming Soon</h2>
                 </div>
-                <div><RightSideNav></RightSideNav></div>
+                <div className='border'>
+                    <RightSideNav></RightSideNav>
+                </div>
             </div>
         </div>
     );
