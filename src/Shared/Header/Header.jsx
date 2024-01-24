48cf54f0f4cd04cda3907c2161b4edb613797a19
imrulkayes75 <ikbubt@gmail.com>
2024-01-24 22:26:22 +0600

    Date Time Formatting Using Moment.Js

--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import logo from '../../assets/logo.png';
+import moment from 'moment/moment';
 
 const Header = () => {
+    const date = moment().format("dddd,MMMM DD, YYYY");
     return (
-        <div className=''>
-            <img src={logo} alt="" />
+        <div className='text-center'>
+            <img className='mx-auto' src={logo} alt="" />
+            <p>Journalism Without Fear or Favour</p>
+            <p>{date}</p>
+
         </div>
     );
 };
